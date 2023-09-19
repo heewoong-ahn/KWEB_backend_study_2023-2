0df317b764f724df234b62fdc750b213b30424cc
heewoong-ahn <102745492+heewoong-ahn@users.noreply.github.com>
2023-09-19 19:40:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+//git clone 주소로 git에 있는것 복제
+//복제한 폴더 안에 변경사항 만들고
+//git add . "."은 all을 의미
+//git commit
+//git push
